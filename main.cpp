c5fae6db80ba9db385d987e1a3ec6cf763303f14
jeremynr <1068730+jeremynr@users.noreply.github.com>
2020-11-07 11:57:11 -0600

    Update main.cpp
    Changed output layout for better formatting.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 #define ll long long // Max # is 9223372036854775807
-using namespace std; 
+using namespace std;
 
-ll modCalc(ll bas, ll exp, ll mod) {
-	ll result = 1;
-	while (exp > 0) {
-		if (exp % 2 == 1)
-			result = (result*bas) % mod;
+// b=base e=exponent, m=modulo
+ll modCalc(ll b, ll e, ll m) {
+	ll r = 1;
+	while (e > 0) {
+		if (e % 2 == 1)
+			r = (r*b) % m;
 		//The lack of this shift is why I was getting negative results
-		exp = exp >> 1;
-		bas = (bas*bas) % mod;
+		e = e >> 1;
+		b = (b*b) % m;
 	}
-	return result;
+	return r;
 }
 
 int main() {
-	for (int i = 0; i < 20; i++) {
-		printf("3 ^ %2i %% 17 = %2i\t", i, modCalc(3, i, 17));
-		printf("12 ^ %2i %% 17 = %2i\t", i, modCalc(12, i, 17));
-		printf("6 ^ %2i %% 17 = %2i\n", i, modCalc(6, i, 17));
-	}
+
+    ll base=3,mod=17,u1pr=15,u2pr=13;
+    ll u1pu=modCalc(base,u1pr,mod);
+    ll u2pu=modCalc(base,u2pr,mod);
+
+    printf("Base: %d\t\t\tMod: %d\n\n",base,mod);
+    printf("User1\t\t\tUer2\n");
+    printf("Private: %d\t\tPrivate: %d\n",u1pr,u2pr);
+    printf("Public: %d\t\tPublic: %d\n\n",u1pu,u2pu);
+    printf("Shared Secret\n");
+    printf("User1: %d\t\tUser2: %d\n",modCalc(u2pu,u1pr,mod),modCalc(u1pu,u2pr,mod));
 
 	return 0;
 }
